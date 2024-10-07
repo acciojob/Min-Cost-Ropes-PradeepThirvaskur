3a6179c3a39e3b9ef87e678f5cdc1a2e69e15860
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-07 13:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	  function mincost(arr){
+     const minheap=[...arr].sort((a,b)=>a-b);
+
+     let totalCost=0;
+
+     while(minheap.length>1){
+        let first = minheap.shift();
+        let second = minheap.shift();
+        let cost = first + second;
+        totalCost += cost;
+        
+        minheap.push(cost);
+        minheap.sort((a,b)=>a-b);
+     }
+     return totalCost;
+
+    }
+    console.log(mincost([9,7,5,4]));
   
 }
 
